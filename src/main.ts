8d10eaf82143dc8e71c0a746c20916d4f4a24d9b
Akash <mahmudakash177@gmail.com>
2024-10-30 19:12:00 +0600

    add cors and github action

--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,73 @@
-
-import 'reflect-metadata'
+import "reflect-metadata";
 import { app, httpServer } from "./app";
 import { server } from "./server";
 import cors from "cors";
 import getUser from "./helpers/getUserFromToken";
 import prisma from "./client/prisma";
-import { graphqlUploadExpress } from 'graphql-upload-ts';
+import { graphqlUploadExpress } from "graphql-upload-ts";
 
 import { expressMiddleware } from "@apollo/server/express4";
 import { json } from "body-parser";
 import express from "express";
 import path from "path";
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import { stripeWebhookFunction } from "./webhook/stripe";
 
 const main = async () => {
-  const startedServer = await server()
-  app.set('trust proxy', true);
+  const startedServer = await server();
+  app.set("trust proxy", true);
 
-	// Disable powered by so people do not know the technology powering the server
-	app.disable('x-powered-by');
+  // Disable powered by so people do not know the technology powering the server
+  app.disable("x-powered-by");
   app.use(morgan("dev"));
-app.use(    cookieParser())
-  await startedServer.start()
+  app.use(cookieParser());
+  await startedServer.start();
   app.post(
-      "/api/webhook",
-      express.raw({ type: "application/json" }),
-      stripeWebhookFunction
-    );
-app.use("/public", express.static(path.join(__dirname, "..", 'public')));
+    "/api/webhook",
+    express.raw({ type: "application/json" }),
+    stripeWebhookFunction
+  );
+  app.use("/public", express.static(path.join(__dirname, "..", "public")));
 
   app.use(
     "/graphql",
     cors<cors.CorsRequest>({
-      origin:['http://localhost:5173' , 'http://localhost:3000', 
-      'https://worldeconomylab.org','https://www.worldeconomylab.org',
-       'https://admin.worldeconomylab.org' , 'https://mathengine.worldeconomylab.org', "http://localhost:3001"],
+      origin: [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://www.artbooking.art",
+        "https://admin.artbooking.art",
+        "http://localhost:3001",
+      ],
       credentials: true,
-
     }),
     json(),
     graphqlUploadExpress(),
 
-
     expressMiddleware(startedServer, {
-      context: async ({ req , res, }) => {
+      context: async ({ req, res }) => {
         const user = await getUser(req.headers.authorization);
-        
-        return { user, token: req.headers.authorization , prisma, req,res , };
+
+        return { user, token: req.headers.authorization, prisma, req, res };
       },
-    
     })
   );
 
-
-const start =async () => {
-  try {
-    await new Promise<void>((resolve) =>
-      httpServer.listen({ port: process.env.PORT || 8000 }, resolve)
-    );
-    console.log(`🚀 Server ready at http://localhost:8000/graphql`);
-
-  } catch (error) {
-    console.log(error);
-    
-  }
-
-}
- return {
-  httpServer , start
- }
-
+  const start = async () => {
+    try {
+      await new Promise<void>((resolve) =>
+        httpServer.listen({ port: process.env.PORT || 8000 }, resolve)
+      );
+      console.log(`🚀 Server ready at http://localhost:8000/graphql`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return {
+    httpServer,
+    start,
+  };
 };
 
- 
-
-
-  export default main;
+export default main;
